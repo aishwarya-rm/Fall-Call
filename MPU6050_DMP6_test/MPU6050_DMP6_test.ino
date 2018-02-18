94e4e720ec7180785aace178146fa33f83ee9994
Michael Yi <michaely1113@gmail.com>
2018-02-17 16:25:22 -0800

    add printing for magnitude of accelorometer data

--- MPU6050_DMP6_test/MPU6050_DMP6_test.ino
+++ MPU6050_DMP6_test/MPU6050_DMP6_test.ino
@@ -45,6 +45,8 @@ THE SOFTWARE.
 // for both classes must be in the include path of your project
 #include "I2Cdev.h"
 
+#include <math.h>
+
 #include "MPU6050_6Axis_MotionApps20.h"
 //#include "MPU6050.h" // not necessary if using MotionApps include file
 
@@ -84,27 +86,27 @@ MPU6050 mpu;
 // uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
 // quaternion components in a [w, x, y, z] format (not best for parsing
 // on a remote host such as Processing or something though)
-//#define OUTPUT_READABLE_QUATERNION
+#define OUTPUT_READABLE_QUATERNION
 
 // uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
 // (in degrees) calculated from the quaternions coming from the FIFO.
 // Note that Euler angles suffer from gimbal lock (for more info, see
 // http://en.wikipedia.org/wiki/Gimbal_lock)
-//#define OUTPUT_READABLE_EULER
+#define OUTPUT_READABLE_EULER
 
 // uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
 // pitch/roll angles (in degrees) calculated from the quaternions coming
 // from the FIFO. Note this also requires gravity vector calculations.
 // Also note that yaw/pitch/roll angles suffer from gimbal lock (for
 // more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
-// #define OUTPUT_READABLE_YAWPITCHROLL
+#define OUTPUT_READABLE_YAWPITCHROLL
 
 // uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
 // components with gravity removed. This acceleration reference frame is
 // not compensated for orientation, so +X is always +X according to the
 // sensor, just without the effects of gravity. If you want acceleration
 // compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
-//#define OUTPUT_READABLE_REALACCEL
+#define OUTPUT_READABLE_REALACCEL
 
 // uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
 // components with gravity removed and adjusted for the world frame of
@@ -292,12 +294,22 @@ void loop() {
             mpu.dmpGetGravity(&gravity, &q);
             mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
             mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+
+            float x =  aaWorld.x;
+            float y = aaWorld.y;
+            float z = aaWorld.z;
+            
             Serial.print("aworld\t");
-            Serial.print(aaWorld.x);
+            Serial.print(x);
             Serial.print("\t");
-            Serial.print(aaWorld.y);
+            Serial.print(y);
             Serial.print("\t");
-            Serial.println(aaWorld.z);
+            Serial.println(z);
+
+            Serial.print("\t");
+
+            // Magnitude of x, y, z from accelerometer 
+            Serial.println(sqrt(sq(x)+ sq(y) + sq(z)));
         #endif
     
        
